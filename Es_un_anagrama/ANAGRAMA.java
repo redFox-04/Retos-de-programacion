f590b18fad10830957b007f0af61a270ad548bf6
LuisR <1327luisrojas@gmail.com>
2022-09-24 08:29:41 -0500

    Se crea repo con dos primeros retos

---
+++ Es_un_anagrama/ANAGRAMA.java
@@ -0,0 +1,19 @@
+// Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+// Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+// NO hace falta comprobar que ambas palabras existan.
+// Dos palabras exactamente iguales no son anagrama.
+
+
+package Es_un_anagrama;
+
+import java.util.ArrayList;
+
+public class ANAGRAMA {
+
+public static void name(String[] args) {
+    ArrayList<String> posibles_anagramas = new ArrayList<String>();
+
+    posibles_anagramas.add("")
+}    
+}
+
